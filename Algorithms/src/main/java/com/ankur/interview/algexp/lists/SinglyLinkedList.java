fd6b88e0493d2ec15f69afff3fe625f18811fefd
ankurbrdwj <ankurbrdwj@gmail.com>
2021-09-13 23:24:26 +0530

    practice
    practice

--- Algorithms/src/main/java/com/ankur/interview/algexp/lists/SinglyLinkedList.java
+++ Algorithms/src/main/java/com/ankur/interview/algexp/lists/SinglyLinkedList.java
@@ -1,4 +1,12 @@
 package com.ankur.interview.algexp.lists;
 
 public class SinglyLinkedList {
+    public int value;
+    public SinglyLinkedList next;
+
+    public SinglyLinkedList(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
 }
