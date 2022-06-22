98968ac03b54cd5bbd6176e299100a827d485fa6
Zell Liew <zellwk@gmail.com>
2022-06-22 11:29:09 +0200

    removed duplicate project

--- Algorithms 2/src/main/java/com/ankur/interview/sorting/elementarysorts/Util.java
+++
@@ -1,9 +0,0 @@
-package com.ankur.interview.sorting.elementarysorts;
-
-public class Util {
-    public static void swap(int i, int i1, int[] a) {
-        int temp = a[i];
-        a[i] = a[i1];
-        a[i1] = temp;
-    }
-}
