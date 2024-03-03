18e51fa3b2f1bf548ea1c6e5dd8a6bcf6d5060aa
Ankur Bhardwaj <ankurbrdwj@gmail.com>
2024-03-03 17:28:32 +0100

    Merge Sorted linked lists

--- Algorithms/src/main/java/com/ankur/interview/practice/lists/MergeSortedLinkedLists.java
+++ Algorithms/src/main/java/com/ankur/interview/practice/lists/MergeSortedLinkedLists.java
@@ -1,4 +1,5 @@
 package com.ankur.interview.practice.lists;
+
 public class MergeSortedLinkedLists {
   // This is an input class. Do not edit.
   public static class LinkedList {
@@ -16,48 +17,55 @@ public class MergeSortedLinkedLists {
     LinkedList l1 = headOne;
     LinkedList l2 = headTwo;
     LinkedList prev = null;
-    printList(headOne,"headOne");
-    printList(headTwo,"headTwo");
-    printList(l1,"FirstPointer");
-    printList(l2,"SecondPointer");
+    printList(headOne, "headOne");
+    printList(headTwo, "headTwo");
+    printList(l1, "FirstPointer");
+    printList(l2, "SecondPointer");
     System.out.println("=================================");
     while (l1 != null && l2 != null) {
       if (l1.value < l2.value) {
-        System.out.println("First Pointer: "+l1.value +" is smaller than Second Pointer: "+ l2.value);
-        if(prev!=null)
-        System.out.println("Current smaller "+prev.value+"will become -->"+ l1.value);
+        System.out.println(
+          "First Pointer: " + l1.value + " is smaller than Second Pointer: " + l2.value);
+        if (prev != null) {
+          System.out.println("Current smaller " + prev.value + "will become -->" + l1.value);
+        }
         prev = l1;
-        if( l1.next!=null)
-        System.out.println("First Pointer "+l1.value+"will become -->"+ l1.next.value);
+        if (l1.next != null) {
+          System.out.println("First Pointer " + l1.value + "will become -->" + l1.next.value);
+        }
         l1 = l1.next;
       } else {
-        System.out.println("Second Pointer: "+l2.value +" is smaller than First Pointer: "+ l1.value);
+        System.out.println(
+          "Second Pointer: " + l2.value + " is smaller than First Pointer: " + l1.value);
         if (prev != null) {
-          System.out.println("## Reassigning smaller "+prev.value+".next to : -->"+ l2.value);
+          System.out.println("## Reassigning smaller " + prev.value + ".next to : -->" + l2.value);
           prev.next = l2;
         }
-        if(prev!=null)
-          System.out.println("Current smaller "+prev.value+"will become -->"+ l2.value);
+        if (prev != null) {
+          System.out.println("Current smaller " + prev.value + "will become -->" + l2.value);
+        }
         prev = l2;
-        if(l2.next!=null)
-          System.out.println("Second Pointer "+l2.value+"will become -->"+ l2.next.value);
+        if (l2.next != null) {
+          System.out.println("Second Pointer " + l2.value + "will become -->" + l2.next.value);
+        }
         l2 = l2.next;
-        System.out.println("<< Reassigning smaller "+prev.value+".next to : -->"+ l1.value);
+        System.out.println("<< Reassigning smaller " + prev.value + ".next to : -->" + l1.value);
         prev.next = l1;
       }
-      printList(prev,"smallerNode");
-      printList(l1,"FirstPointer");
-      printList(l2,"SecondPointer");
-      printList(headOne,"headOne");
-      printList(headTwo,"headTwo");
+      printList(prev, "smallerNode");
+      printList(l1, "FirstPointer");
+      printList(l2, "SecondPointer");
+      printList(headOne, "headOne");
+      printList(headTwo, "headTwo");
       System.out.println("=================================");
     }
-    if(l1==null)
-      prev.next=l2;
+    if (l1 == null) {
+      prev.next = l2;
+    }
     System.out.println("=================================");
-    printList(headOne,"headOne");
-    printList(headTwo,"headTwo");
-    return headOne.value<headTwo.value?headOne:headTwo;
+    printList(headOne, "headOne");
+    printList(headTwo, "headTwo");
+    return headOne.value < headTwo.value ? headOne : headTwo;
   }
 
   public static void printList(LinkedList node, String headName) {
@@ -86,11 +94,12 @@ public class MergeSortedLinkedLists {
     }
     return node;
   }
+
   public static void main(String[] args) {
-    LinkedList l1 = createList(new int[]{1,5,7,9},4);
-    LinkedList l2 = createList(new int[]{2,3,6,8},4);
-    printList(l1,"input1");
-    printList(l2,"input2");
-    mergeLinkedLists(l1,l2);
+    LinkedList l1 = createList(new int[] {1, 5, 7, 9}, 4);
+    LinkedList l2 = createList(new int[] {2, 3, 6, 8}, 4);
+    printList(l1, "input1");
+    printList(l2, "input2");
+    mergeLinkedLists(l1, l2);
   }
 }
